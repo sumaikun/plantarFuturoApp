a627cf645c56add9d5a00cb94784c7c194d7ad38
sumaikun <ventas.javc@gmail.com>
2019-07-16 23:42:31 -0500

    fixing bugs in offline for risk management

--- src/flux/actions/RiskManagement/RiverCollapseActions.js
+++ src/flux/actions/RiskManagement/RiverCollapseActions.js
@@ -13,6 +13,16 @@ import Ons from 'onsenui';
 export const createRiverCollapse = (data,successCallBack  ,errorCallBack) => {
   return async dispatch => {
 
+    if(!navigator.onLine)
+    {
+      console.log("Modo offline");
+      data.created_at = new Date().toISOString().split('T')[0];
+      dispatch(addOfflineRiverColl(data));
+      Ons.notification.alert({title:"¡Que bien!",message:"¡Inspección de quebrada guardada en memoria!"});
+      dispatch(goBack());
+      return;
+    }
+
     dispatch(fetching());
 
     let SuccessCallBack = successCallBack ? successCallBack : (response) => {
@@ -40,6 +50,28 @@ export const createRiverCollapse = (data,successCallBack  ,errorCallBack) => {
 
 export const updateRiverCollapse = (id,data,successCallBack  ,errorCallBack) => {
   return async dispatch => {
+
+
+    if(!navigator.onLine)
+    {
+      console.log("Modo offline");
+
+      if(!data.ToSynchro)
+      {
+        console.log("editar del servidor");
+        dispatch(updateServerRiverColl(data));
+        dispatch(goBack());
+      }
+      else
+      {
+        console.log("editar offline");
+        dispatch(updateOfflineRiverColl(data));
+        dispatch(goBack());
+      }
+
+      Ons.notification.alert({title:"¡Que bien!",message:"¡Inspección de quebrada guardado en memoria!"});
+      return;
+    }
 
     dispatch(fetching());
 
