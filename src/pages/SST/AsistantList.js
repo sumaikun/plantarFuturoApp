09a45db107da93cd95256192540af692e03b2168
miigangls <miigangls@gmail.com>
2019-07-25 09:42:56 -0500

    task086 - create modal.

--- src/pages/SST/AsistantList.js
+++ src/pages/SST/AsistantList.js
@@ -6,15 +6,15 @@ import { workingRowStyles, modalStyles } from "../../jsStyles/Styles";
 
 
 //Onsen Ui
-import {  List , ListItem, Col, Row, Checkbox, ListHeader, Radio, Input } from 'react-onsenui';
+import {  List , ListItem, Col, Row, Checkbox, ListHeader, Radio, Input, Modal, Button} from 'react-onsenui';
 import Ons from 'onsenui';
 
 //Libraries
 
 //components
 import Loading from "../../components/Loading";
-import Modal from "../../components/Modal";
-import ModalAssitant from "../../components/modalAssitant";
+//import Modal from "../../components/Modal";
+//import ModalAssitant from "../../components/modalAssitant";
 
 //container
 import AppPage from '../../containers/AppPage';
@@ -83,12 +83,37 @@ workingRowStyles.listText = {
 class AsistantList extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      modalAssitant:false,
+      modalAbsence: false
+    }
     this.renderHeader = this.renderHeader.bind(this);
-    this.contentPage = this.contentPage.bind(this);
   }
   componentDidMount(){
     this.props.getList(this.props.project_id);
     this.props.getListUsers(this.props.project_id)
+  }
+  onOpenAssiten(){
+    this.setState({modalAssitant: true})
+  }
+  opOpenAbsence(){
+    this.setState({modalAbsence: true})
+  }
+  onCloseAssiten(){
+    this.setState({modalAssitant: false})
+  }
+  opCloseAbsence(){
+    this.setState({modalAbsence: false})
+  }
+  onClickVisitorForm(memo) {
+    console.log(memo);
+    this.props.getSSTVisitor(memo, 'update')
+    this.props.goToAssistantForm()
+  }
+
+  onClickSSTCreate() {
+    this.props.getSSTVisitor({})
+    this.props.goToAssistantForm()
   }
   renderHeader(){
     return(
@@ -109,163 +134,173 @@ class AsistantList extends Component {
       </ListHeader>
     );
   }
-
-  contentPage(){
-    let {listSSTVisitors, listSSTAssistants, listUser}= this.props.appState, listData = []
+  render() {
+    let  {  isFetching, listSSTVisitors, listSSTAssistants, listUser } = this.props.appState,  listData = []
     if (listSSTVisitors) listData.push(...listSSTVisitors);
     if (listSSTAssistants)  listData.push(...listSSTAssistants);
     if(listUser) listData.push(...listUser);
-    return(
-      <div>
-        <div style={styles.formContainer}>
-          <div className="login-form" >
-            <div className="group" style={styles.searchInputContainer}>
-                <input id="search" /*value={searchName} name="buscador" onChange={e => this.setState({ searchName: e.target.value })} */className="input fontAwesome" placeholder="Buscar" type="text"   style={{fontFamily:'Arial', marginTop:"8px", width:"9GO_TO_MACHINERY_LIST0%", height:"10px"}} /><br />
-              <div style={styles.searchButton} onClick={()=>{this.onClickSSTCreate()}}>
-                  <span className="fas fa-plus fontAwesome" ></span>
+    return (
+      <AppPage  title={["Lista de ", <strong>Asistentes</strong>]} backButton={true} >
+        <div>
+          <div style={styles.formContainer}>
+            <div className="login-form" >
+              <div className="group" style={styles.searchInputContainer}>
+                  <input id="search" /*value={searchName} name="buscador" onChange={e => this.setState({ searchName: e.target.value })} */className="input fontAwesome" placeholder="Buscar" type="text"   style={{fontFamily:'Arial', marginTop:"8px", width:"9GO_TO_MACHINERY_LIST0%", height:"10px"}} /><br />
+                <div style={styles.searchButton} onClick={()=>{this.onClickSSTCreate()}}>
+                    <span className="fas fa-plus fontAwesome" ></span>
+                </div>
               </div>
-            </div>
+          </div>
         </div>
-      </div>
-          <List renderHeader={this.renderHeader}>
-          {
-            listData.map((memo, i) => {
-                return  (
-                <div>
-                  <ListItem>
+            <List renderHeader={this.renderHeader}>
+            {
+              listData.map((memo, i) => {
+                  return  (
+                  <div>
+                    <ListItem>
 
-                  <Col width="50%">
-                    <div onClick={()=>{this.onClickVisitorForm(memo)}} className="center" style={styles.mainListItem}>
-                      <span style={styles.counter}>{i+1}</span>
-                      <span style={styles.projectName}>{memo.name}</span>
-                      <div>
-                        <span style={styles.projectInfo}>{memo.state}</span>
-                      </div>
-                      <div style={styles.buttonContainer}>
-                        <div style={styles.ProjectButton}>
-                          <i className="fas fa-arrow-right fontAwesome"></i>
+                    <Col width="50%">
+                      <div onClick={()=>{this.onClickVisitorForm(memo)}} className="center" style={styles.mainListItem}>
+                        <span style={styles.counter}>{i+1}</span>
+                        <span style={styles.projectName}>{memo.name}</span>
+                        <div>
+                          <span style={styles.projectInfo}>{memo.state}</span>
+                        </div>
+                        <div style={styles.buttonContainer}>
+                          <div style={styles.ProjectButton}>
+                            <i className="fas fa-arrow-right fontAwesome"></i>
+                          </div>
                         </div>
                       </div>
+                    </Col>
+                    <Col width="25%" style={{textAlign:"center"}}>
+                      <Radio onChange={()=>{this.onOpenAssiten()}} id="modal-btn-Assistant" name="test" style={ workingRowStyles.radioCircle } />
+                    </Col>
+                    <Col width="25%" style={{textAlign:"center"}}>
+                      <Radio onChange={()=> {this.opOpenAbsence()}} id="modal-btn" name="test" style={ workingRowStyles.radioCircle } />
+                    </Col>
+                    </ListItem>
+                      <div style={{
+                          height: "10px",
+                          backgroundColor: "#e6e7e8",
+                      }}>
+                      </div>
                     </div>
-                  </Col>
-                  <Col width="25%" style={{textAlign:"center"}}>
-                    <Radio id="modal-btn-Assistant" name="test" style={ workingRowStyles.radioCircle } />
-                  </Col>
-                  <Col width="25%" style={{textAlign:"center"}}>
-                    <Radio id="modal-btn" name="test" style={ workingRowStyles.radioCircle } />
-                  </Col>
-                  </ListItem>
-                    <div style={{
-                        height: "10px",
-                        backgroundColor: "#e6e7e8",
-                    }}>
-                    </div>
-                  </div>
-                );
-            })
-          }
-          </List>
-        <div style={{overflow:'hidden',marginTop:"130%", backgroundColor: 'orange' }}>
-          <div className="group" style={{...styles.searchInputContainer, "justify-content":"left"}}>
-            <div style={styles.buttonContainer}>
-              <div style={styles.ProjectButton} >
-                <i className="fas fa-pencil-alt fontAwesome"></i>
+                  );
+              })
+            }
+            </List>
+          <div style={{overflow:'hidden',marginTop:"130%", backgroundColor: 'orange' }}>
+            <div className="group" style={{...styles.searchInputContainer, "justify-content":"left"}}>
+              <div style={styles.buttonContainer}>
+                <div style={styles.ProjectButton} >
+                  <i className="fas fa-pencil-alt fontAwesome"></i>
+                </div>
               </div>
-            </div>
-            <div style={{ ...styles.buttonContainer, 'margin-left':'50%' }}>
-              <div style={styles.ProjectButton}>
-                <i className="far trash-alt fontAwesome"></i>
+              <div style={{ ...styles.buttonContainer, 'margin-left':'50%' }}>
+                <div style={styles.ProjectButton}>
+                  <i className="far trash-alt fontAwesome"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  onClickVisitorForm(memo) {
-    console.log(memo);
-    this.props.getSSTVisitor(memo, 'update')
-    this.props.goToAssistantForm()
-  }
-  
-  onClickSSTCreate() {
-    this.props.getSSTVisitor({})
-    this.props.goToAssistantForm()
-  }
-  render() {
-    const {  isFetching } = this.props.appState
 
-    return (
-      <AppPage  title={["Lista de ", <strong>Asistentes</strong>]} backButton={true} >
+        {this.modalAssitant()}
 
-          {  isFetching ?
-            <div style={{backgroundColor:"white",height:"100%"}}>
-              <Loading/>
-            </div> :
-             this.contentPage()
-          }
+        {this.modalAbsence()}
 
-
-          <Modal title="" ModalStyles={{...styles.modalStyles, ...styles.modalOverride  }}>
-              <Row style={ styles.modalCell }>
-                <div>MOTIVO AUSENCIA</div>
-              </Row>
-
-              <Row>
-                <Col style={ styles.modalCell }>
-                  <span>Incapacidad</span>
-                </Col>
-                <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
-                  <Radio name="excuse"  />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col style={ styles.modalCell }>
-                  <span>Permiso</span>
-                </Col>
-                <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
-                  <Radio name="excuse"  />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col style={ styles.modalCell }>
-                  <span>Injustificado</span>
-                </Col>
-                <Col  width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
-                  <Radio name="excuse"  />
-                </Col>
-              </Row>
-          </Modal>
-          
-          <ModalAssitant title="" ModalStyles={{...styles.modalStyles, ...styles.modalOverride  }}>
-              <Row style={ styles.modalCell }>
-                <div>REGISTRO DE HORAS DE TRABAJO</div>
-              </Row>
-
-              <Row>
-                <Col style={ styles.modalCell }>
-                  <span>Entrada: </span>
-                </Col>
-                <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
-                  <Input name="excuse"  />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col style={ styles.modalCell }>
-                  <span>Salida</span>
-                </Col>
-                <Col  width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
-                  <Input name="excuse"  />
-                </Col>
-              </Row>
-          </ModalAssitant>
 
       </AppPage>
     );
   }
+
+  modalAssitant(){
+    return(
+      <Modal isOpen={this.state.modalAssitant} animation="fade">
+        <Row style={ styles.modalCell }>
+          <div>REGISTRO DE HORAS DE TRABAJO</div>
+        </Row>
+
+        <Row>
+          <Col style={ styles.modalCell }>
+            <span>Entrada: </span>
+          </Col>
+          <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Input name="excuse"  />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={ styles.modalCell }>
+            <span>Salida</span>
+          </Col>
+          <Col  width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Input name="excuse"  />
+          </Col>
+        </Row>
+        <Row>
+          <Col  width="50%" style={ styles.modalCell }>
+            <Button modifier="large--cta">
+                REGISTRAR
+            </Button>
+          </Col>
+          <Col  width="50%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Button onClick={()=>{this.onCloseAssiten()}}   modifier="large--cta">CERRAR</Button>
+          </Col>
+        </Row>
+      </Modal>
+    )
+  }
+
+  modalAbsence(){
+    return(
+      <Modal style={{marginLeft:"1em", marginRight:"1em"}} isOpen={this.state.modalAbsence}>
+        <Row style={ styles.modalCell }>
+          <div>MOTIVO AUSENCIA</div>
+        </Row>
+
+        <Row>
+          <Col style={ styles.modalCell }>
+            <span>Incapacidad</span>
+          </Col>
+          <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Radio name="excuse"  />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={ styles.modalCell }>
+            <span>Permiso</span>
+          </Col>
+          <Col width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Radio name="excuse"  />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={ styles.modalCell }>
+            <span>Injustificado</span>
+          </Col>
+          <Col  width="25%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Radio name="excuse"  />
+          </Col>
+        </Row>
+        <Row>
+          <Col width="50%" style={ styles.modalCell }>
+            <Button modifier="large--cta">
+              REGISTRAR
+            </Button>
+          </Col>
+          <Col onClick={()=>{this.opCloseAbsence()}}  width="50%" style={{ ...styles.modalCell , ...styles.modalColumn }} >
+            <Button modifier="large--cta">CERRAR</Button>
+          </Col>
+        </Row>
+
+      </Modal>
+    )
+  }
+
 }
 const mapStateToProps = state => {
 
